96a4468b8b1925f024f6ee96514cd437f475c8b8
Shirren Premaratne <shirren.premaratne@gmail.com>
2015-06-02 22:54:58 +1000

    Completed the demo

--- 03_Collections_Loops/starter_code/code_demo_array.rb
+++ 03_Collections_Loops/starter_code/code_demo_array.rb
@@ -1,20 +1,38 @@
 #Arrays
 #TIME: 15 min
-#INSTRUCTIONAL DESIGN NOTES: 
-# => This code along is meant to be completed in irb. 
-# => We want students to have a general understanding of arrays. 
-# => Show how they can find examples and more methods for arrays in the ruby docs.
-
 
 # Create a new array and assign it to a variable
+my_arr = []
+
 # Check its class
+my_arr.class
+
 # Reiterate that a class tells you what things this object can respond to
 # Create a staff array with the instructor and TAs names
+staff = ['Shirren', 'Ben']
+
 # Show getting size with count, size, length, empty?
+puts staff.count
+puts staff.size
+puts staff.length
+puts staff.empty?
+
 # Show reading from an array, zero indexing
+puts staff[0]
+puts staff[1]
+
 # Show appending to the array, with push, << and unshift
+staff.push('Zac')
+staff << 'Mikaela'
+staff.unshift('Nikki')
+
 # Show popping and shifting
+staff.pop
+staff.shift
+
 # Show how include? works
-# Show BANG Methods #sort!, #uniq!, #shuffle!, reverse! 
-# Show join, to_s
-# Show split from string
+staff.include?('Zac')
+staff.include?('Shirren')
+
+# Try BANG Methods #sort!, #uniq!, #shuffle!, reverse!
+# Try join, to_s
